d6918c29431635ce0019bd47337872748b445187
EkoEkoMariaaaaa <EkoEkoMariaaaaa>
2016-09-03 23:19:53 +0200

    Add correct workspace
    Tihi

---
+++ PokeSpace/Pokemon/src/Types/GrassPokemon.java
@@ -0,0 +1,14 @@
+package Types;
+
+import Base.Pokemon;
+
+public abstract class GrassPokemon extends Pokemon {
+
+	
+	
+	public GrassPokemon(String name, int maxHealth) {
+		super(name, maxHealth);
+		// new Pokemon("Fredrik", 155)
+	}
+	
+}
