eabdb369d8bee1454f76619c9e8d8c4ac11df04f
Tinglans <eko12ms1@student.lu.se>
2016-09-03 21:04:43 +0200

    Add files via upload

---
+++ WaterPokemon.java
@@ -0,0 +1,30 @@
+package Types;
+
+import Base.Pokemon;
+
+public abstract class WaterPokemon extends Pokemon {
+
+	public WaterPokemon(String nameFromConstructor, int maxHealth) {
+		super(nameFromConstructor, maxHealth);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void Eat(Food food) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void Damage(int damage) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void Attack(Pokemon pokemon, String attackName) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
